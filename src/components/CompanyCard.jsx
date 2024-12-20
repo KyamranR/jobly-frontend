40e8bf93f37b255aef962a0e9a8d29febc042da4
Kyamran <kyamranr@gmail.com>
2024-12-19 18:44:42 -0600

    fix the issue with company logo and added css to display properly

--- src/components/CompanyCard.jsx
+++ src/components/CompanyCard.jsx
@@ -1,9 +1,15 @@
 import { Link } from "react-router-dom";
 
 function CompanyCard({ handle, name, logoUrl }) {
+  const fallbackUrl = logoUrl || "/vite.svg";
+
   return (
     <div className="CompanyCard">
-      {<img src={logoUrl} />}
+      <img
+        src={fallbackUrl}
+        alt="Company logo"
+        onError={(e) => (e.target.src = "/vite.svg")}
+      />
       <h3>
         <Link to={`/companies/${handle}`}>{name}</Link>
       </h3>
